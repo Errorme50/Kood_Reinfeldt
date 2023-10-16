b014bee8683f385d662963d2e907be4b08873eab
Errorme50 <reinfeldtarthur@gmail.com>
2023-10-16 17:27:38 +0300

    commit

---
+++ js_harjutused/main.js
@@ -0,0 +1,10 @@
+function harjutus1(nimi){
+    console.log("Tere tulemast " + nimi);
+}
+function harjutus2(mark , mudel , värv){
+    console.log("Minu auto on " + värv + " " + mark + " " + mudel + ".");
+}
+function harjutus3(pindala , kõrgus){
+    let ruumala = pindala * kõrgus / 3
+    console.log("Ruumala on " + ruumala);
+}
