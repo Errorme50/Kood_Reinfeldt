bf5546ddee294c887d1f297aa9d038f13c42889a
Errorme50 <reinfeldtarthur@gmail.com>
2023-10-30 16:25:52 +0200

    commit

--- js_harjutused/main.js
+++ js_harjutused/main.js
@@ -60,3 +60,62 @@ function harjutus12(){
         i++;
     }
 }
+function harjutus13(massiiv) {
+    if (massiiv.includes("Marek")) {
+        console.log("Marek on massiivis");
+    } else {
+        console.log("Marek pole massiivis");
+    }
+}
+let inimesed = [
+    {
+        nimi: 'Alice',
+        vanus: 28,
+        aadress: 'Pargi 7',
+        telefon: '23857493',
+        email: 'alice@eesti.ee'
+    },
+    {
+        nimi: 'Bob',
+        vanus: 32,
+        aadress: 'Tänavaküla 12',
+        telefon: '293463847',
+        email: 'bob@gmail.com'
+    },
+    {
+        nimi: 'Marek',
+        vanus: 22,
+        aadress: 'Metsatee 3',
+        telefon: '8594032',
+        email: 'marek@example.com'
+    }
+];
+
+function harjutus14(inimesed) {
+    let vanim = inimesed[0];
+    let pikimAadress = inimesed[0];
+    let lühimTelefon = inimesed[0];
+    let eestiEmail = null;
+
+    for (let inimene of inimesed) {
+        if (inimene.vanus > vanim.vanus) {
+            vanim = inimene;
+        }
+        if (inimene.aadress.length > pikimAadress.aadress.length) {
+            pikimAadress = inimene;
+        }
+        if (inimene.telefon.length < lühimTelefon.telefon.length) {
+            lühimTelefon = inimene;
+        }
+        if (inimene.email.includes("eesti")) {
+            eestiEmail = inimene;
+        }
+    }
+
+    console.log("Kõige vanem: " + vanim.nimi);
+    console.log("Pikima aadressiga: " + pikimAadress.nimi);
+    console.log("Lühima telefoninumbriga: " + lühimTelefon.nimi);
+    if (eestiEmail) {
+        console.log("E-post sisaldab sõna 'eesti': " + eestiEmail.nimi);
+    }
+}
