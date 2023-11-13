45cbcd1a3d571d4c0ad723f5d128256d967d555d
Errorme50 <reinfeldtarthur@gmail.com>
2023-11-13 15:22:45 +0200

    commit

--- 8.Tund/mainShopping.js
+++ 8.Tund/mainShopping.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("addItem").onclick = function(){
-        document.createElement("shopping-list") = document.getElementById("item").value;
-        document.appendChild("shopping-list");
+    document.getElementById("addItem").onclick = function() {
+        const newParagraph = document.createElement('p');
+        let myul = document.getElementById("shopping-list");
+        
+        newParagraph.textContent = document.getElementById("item").value;
+        myul.appendChild(newParagraph);
     }
 })
